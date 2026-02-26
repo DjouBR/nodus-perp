ffd64d04eefb2a9c4f3fe3095483106543b9bfb0
DjouBR <95130488+DjouBR@users.noreply.github.com>
2026-02-25 21:39:37 -0300

    fix: add .js extensions to schema/index.js exports for ESM compatibility

--- src/lib/db/schema/index.js
+++ src/lib/db/schema/index.js
@@ -1,10 +1,10 @@
 // Exportação centralizada de todos os schemas
 // Importar de '@/lib/db/schema' no código da aplicação
 
-export * from './tenants'
-export * from './users'
-export * from './sessions'
-export * from './monitoring'
-export * from './training'
-export * from './gamification'
-export * from './financial'
+export * from './tenants.js'
+export * from './users.js'
+export * from './sessions.js'
+export * from './monitoring.js'
+export * from './training.js'
+export * from './gamification.js'
+export * from './financial.js'
